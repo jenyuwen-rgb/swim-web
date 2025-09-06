b636dbe5e1c723c197252fb1b5cf560c0065bac3
JYWEN <jywen@JYWENdeMacBook-Air.local>
2025-09-06 20:51:30 +0800

    chore: wire NEXT_PUBLIC_API_URL

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+const NEXT_PUBLIC_API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://swimming-record.onrender.com";
+
+module.exports = {
+  reactStrictMode: true,
+  env: { NEXT_PUBLIC_API_URL },
+};
